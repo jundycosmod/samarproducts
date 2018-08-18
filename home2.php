1c5fd58cc99d5befadc5a34af84ecf5454a0d09a
jcagentzero <jundycosmod@gmail.com>
2018-08-18 22:47:34 +0800

    Converted some modules compatible to PHP 7
    Signed-off-by: jcagentzero <jundycosmod@gmail.com>

--- home2.php
+++ home2.php
@@ -1,68 +1,67 @@
-<?php require_once('Connections/akonsudoy.php'); ?>
+<?php require_once 'Connections/akonsudoy.php';?>
 <?php
 // *** Validate request to login to this site.
 if (!isset($_SESSION)) {
-  session_start();
+	session_start();
 }
 // ** Logout the current user. **
-$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
-if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
-  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+$logoutAction = $_SERVER['PHP_SELF'] . "?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")) {
+	$logoutAction .= "&" . htmlentities($_SERVER['QUERY_STRING']);
 }
 
-if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
-  //to fully log out a visitor we need to clear the session varialbles
-  $_SESSION['MM_Username'] = NULL;
-  $_SESSION['user_id'] = NULL;
-  $_SESSION['MM_UserGroup'] = NULL;
-  $_SESSION['PrevUrl'] = NULL;
-  unset($_SESSION['MM_Username']);
-  unset($_SESSION['user_id']);
-  unset($_SESSION['MM_UserGroup']);
-  unset($_SESSION['PrevUrl']);
+if ((isset($_GET['doLogout'])) && ($_GET['doLogout'] == "true")) {
+	//to fully log out a visitor we need to clear the session varialbles
+	$_SESSION['MM_Username'] = NULL;
+	$_SESSION['user_id'] = NULL;
+	$_SESSION['MM_UserGroup'] = NULL;
+	$_SESSION['PrevUrl'] = NULL;
+	unset($_SESSION['MM_Username']);
+	unset($_SESSION['user_id']);
+	unset($_SESSION['MM_UserGroup']);
+	unset($_SESSION['PrevUrl']);
 }
 ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") {
+		if (PHP_VERSION < 6) {
+			$theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+		}
 
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+		$theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
 
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
+		switch ($theType) {
+		case "text":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "long":
+		case "int":
+			$theValue = ($theValue != "") ? intval($theValue) : "NULL";
+			break;
+		case "double":
+			$theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+			break;
+		case "date":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "defined":
+			$theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+			break;
+		}
+		return $theValue;
+	}
 }
 
 $email_user_id = "0";
 if (isset($_GET['email'])) {
-  $email_user_id = $_GET['email'];
+	$email_user_id = $_GET['email'];
 }
-mysql_select_db($database_akonsudoy, $akonsudoy);
-$query_user_id = sprintf("SELECT user_id FROM users WHERE %s = %s", GetSQLValueString($email_user_id, "text"),GetSQLValueString($email_user_id, "text"));
-$user_id = mysql_query($query_user_id, $akonsudoy) or die(mysql_error());
-$row_user_id = mysql_fetch_assoc($user_id);
-$totalRows_user_id = mysql_num_rows($user_id);
+mysqli_select_db($akonsudoy, $database_akonsudoy);
+$query_user_id = sprintf("SELECT user_id FROM users WHERE %s = %s", GetSQLValueString($email_user_id, "text"), GetSQLValueString($email_user_id, "text"));
+$user_id = mysqli_query($akonsudoy, $query_user_id) or die(mysql_error());
+$row_user_id = mysqli_fetch_assoc($user_id);
+$totalRows_user_id = mysqli_num_rows($user_id);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -75,5 +74,5 @@ $totalRows_user_id = mysql_num_rows($user_id);
 </body>
 </html>
 <?php
-mysql_free_result($user_id);
+mysqli_free_result($user_id);
 ?>
--- user_login.php
+++ user_login.php
@@ -1,88 +1,86 @@
-<?php require_once('Connections/akonsudoy.php'); ?>
+<?php require_once 'Connections/akonsudoy.php';?>
 <?php
 if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
+	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") {
+		if (PHP_VERSION < 6) {
+			$theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+		}
 
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+		$theValue = function_exists("mysql_real_escape_string") ? mysqli_real_escape_string($GLOBALS['akonsudoy'], $theValue) : mysqli_escape_string($GLOBALS['akonsudoy'], $theValue);
 
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
+		switch ($theType) {
+		case "text":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "long":
+		case "int":
+			$theValue = ($theValue != "") ? intval($theValue) : "NULL";
+			break;
+		case "double":
+			$theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+			break;
+		case "date":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "defined":
+			$theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+			break;
+		}
+		return $theValue;
+	}
 }
 ?>
 <?php
 // *** Validate request to login to this site.
 if (!isset($_SESSION)) {
-  session_start();
+	session_start();
 }
 
 $loginFormAction = $_SERVER['PHP_SELF'];
 if (isset($_GET['accesscheck'])) {
-  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+	$_SESSION['PrevUrl'] = $_GET['accesscheck'];
 }
 $_SESSION['found_user'] = NULL;
 unset($_SESSION['found_user']);
 $_SESSION['found_user'] = true;
 if (isset($_POST['email']) && isset($_POST['button_Login'])) {
-  $loginUsername=$_POST['email'];
-  $password=$_POST['password'];
-  $MM_fldUserAuthorization = "access_level";
-  $MM_redirectLoginSuccess = "index.php";
-  $MM_redirectLoginFailed = "index2.php";
-  $MM_redirecttoReferrer = true;
-  mysql_select_db($database_akonsudoy, $akonsudoy);
-  	
-  $LoginRS__query=sprintf("SELECT email, password, access_level FROM users WHERE email=%s AND password=%s",
-  GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
-   
-  $LoginRS = mysql_query($LoginRS__query, $akonsudoy) or die(mysql_error());
-  $loginFoundUser = mysql_num_rows($LoginRS);
-  if ($loginFoundUser) {
-    
-    $loginStrGroup  = mysql_result($LoginRS,0,'access_level');
-mysql_select_db($database_akonsudoy, $akonsudoy);
-$query_user_id = "SELECT * FROM users WHERE email='".$loginUsername."'";
-$user_id = mysql_query($query_user_id, $akonsudoy) or die(mysql_error());
-$row_user_id = mysql_fetch_assoc($user_id);
-$totalRows_user_id = mysql_num_rows($user_id);
-mysql_free_result($user_id);   
-    //declare two session variables and assign them
-	$_SESSION['user_id'] = $row_user_id['user_id'];
-	$_SESSION['access_level'] = $row_user_id['access_level'];
-    $_SESSION['MM_Username'] = $loginUsername;
-    $_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+	$loginUsername = $_POST['email'];
+	$password = $_POST['password'];
+	$MM_fldUserAuthorization = "access_level";
+	$MM_redirectLoginSuccess = "index.php";
+	$MM_redirectLoginFailed = "index2.php";
+	$MM_redirecttoReferrer = true;
+	mysql_select_db($database_akonsudoy, $akonsudoy);
 
-    if (isset($_SESSION['PrevUrl']) && true) {
-      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
-    }
-    //header("Location: " . $MM_redirectLoginSuccess );
-  }
-  else {
-	  $_SESSION['found_user'] = false;
-	  
-    //header("Location: ". $MM_redirectLoginFailed );
-  }
+	$LoginRS__query = sprintf("SELECT email, password, access_level FROM users WHERE email=%s AND password=%s",
+		GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text"));
+
+	$LoginRS = mysql_query($LoginRS__query, $akonsudoy) or die(mysql_error());
+	$loginFoundUser = mysql_num_rows($LoginRS);
+	if ($loginFoundUser) {
+
+		$loginStrGroup = mysql_result($LoginRS, 0, 'access_level');
+		mysql_select_db($database_akonsudoy, $akonsudoy);
+		$query_user_id = "SELECT * FROM users WHERE email='" . $loginUsername . "'";
+		$user_id = mysql_query($query_user_id, $akonsudoy) or die(mysql_error());
+		$row_user_id = mysql_fetch_assoc($user_id);
+		$totalRows_user_id = mysql_num_rows($user_id);
+		mysql_free_result($user_id);
+		//declare two session variables and assign them
+		$_SESSION['user_id'] = $row_user_id['user_id'];
+		$_SESSION['access_level'] = $row_user_id['access_level'];
+		$_SESSION['MM_Username'] = $loginUsername;
+		$_SESSION['MM_UserGroup'] = $loginStrGroup;
+
+		if (isset($_SESSION['PrevUrl']) && true) {
+			$MM_redirectLoginSuccess = $_SESSION['PrevUrl'];
+		}
+		//header("Location: " . $MM_redirectLoginSuccess );
+	} else {
+		$_SESSION['found_user'] = false;
+
+		//header("Location: ". $MM_redirectLoginFailed );
+	}
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -98,12 +96,12 @@ mysql_free_result($user_id);
     <td colspan="2">
     <div align="center">
   <?php
-  //if ($_SESSION['found_user'] == false) {
-	 // echo "ERROR: username and password did not match!";
- // }else{
-	 // header("Location: index2.php" );
- // }
-  ?>
+//if ($_SESSION['found_user'] == false) {
+// echo "ERROR: username and password did not match!";
+// }else{
+// header("Location: index2.php" );
+// }
+?>
   </div>
     </td>
     </tr>
